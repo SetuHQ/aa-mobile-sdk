fec93a6ff61ed95c03d77235793152acb266f311
Ulhas Mandrawadkar <ulhas.sm@gmail.com>
2024-04-02 16:13:53 +0530

    Adds framework and an empty podspec

---
+++ Klaxon.framework/Headers/Klaxon.h
@@ -0,0 +1,16 @@
+//
+//  Klaxon.h
+//  Klaxon
+//
+//  Created by Ulhas Mandrawadkar on 15/02/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Klaxon.
+FOUNDATION_EXPORT double KlaxonVersionNumber;
+
+//! Project version string for Klaxon.
+FOUNDATION_EXPORT const unsigned char KlaxonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Klaxon/PublicHeader.h>
